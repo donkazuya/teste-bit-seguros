3983190b85d5af45d8aa02a7f6a44c620e1105ed
Donkazuya <danrley.tri@gmail.com>
2025-09-03 20:16:25 -0300

    update 18

--- bit-teste/src/app/app.module.ts
+++ bit-teste/src/app/app.module.ts
@@ -5,21 +5,18 @@ import { NgModule } from '@angular/core';
 import { NgxMaskModule } from 'ngx-mask';
 import { AppComponent } from './app.component';
 import { FormCepComponent } from './form-cep/form-cep.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    FormCepComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    NgxMaskModule.forRoot(),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        FormCepComponent,
+    ],
+    bootstrap: [AppComponent], 
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgxMaskModule.forRoot()
+    ], 
+    providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
